edf8763129294c9014c135586c3c24f1594daaf5
Michael-Bryant Choa <mbchoa@gmail.com>
2017-09-12 10:39:05 -0700

    Change c.js in feature-1-after-merge branch

--- c.js
+++ c.js
@@ -1,1 +1,3 @@
-const string = 'Change c.js in master branch';
+const string = 'Change c.js in feature-1-after-merge branch';
+
+const f1AfterMerge = 'New string added in feature-1-after-merge branch';
