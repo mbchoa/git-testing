33b7ed3847516ed4857e5958cbcd2424622d2c4f
Michael-Bryant Choa <mbchoa@gmail.com>
2017-09-12 10:38:05 -0700

    Change a.js in feature-1-after-merge-branch

--- a.js
+++ a.js
@@ -1,1 +1,3 @@
-const string = 'Change a.js in master branch';
+const string = 'Change a.js in feature-1-after-merge branch';
+
+const newString = 'Another new string added in feature-1-after-merge branch';
